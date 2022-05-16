ddac4c9c5778cd6eb97553a75fc167a2f9737ebb
Chipsa12 <ivango23456@gmail.com>
2022-05-16 07:22:06 +0400

    change deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const API_URL = `http://localhost:5000/`
+export const API_URL = `http://45.90.35.187:5000/`
 export const API_WEATHER_BIT = '36409b78283a4712b2a7a0516373b445'
 export const MAP_BOX_TOKEN = `pk.eyJ1IjoiY2hpcHNhMTIiLCJhIjoiY2wydTZoN2FrMDR0YjNqcGh2eTRjZTgyeCJ9.e--CfxdhU96ymIk9PEjtLQ`
